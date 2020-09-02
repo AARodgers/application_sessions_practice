28c116b6e78c918aac29c44d9222033bb67e2827
github username <github email address>
2020-09-02 16:38:07 -0600

    adds /posts/new route to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,16 @@
 class PostsController < ApplicationController
 
     get '/posts' do
-        "A list of publically available posts"
-        # "You are logged in as #{session[:email]}"
+      "A list of publically available posts"
+      # "You are logged in as #{session[:email]}"
     end
 
+    get '/posts/new' do
+      # checks to see if they are logged in ( won't be if there is no email)
+      if !session[:email]
+          redirect '/login' # redirect if they aren't logged in
+      else
+          "a new post form" #rendering if they are
+      end
+    end
 end
